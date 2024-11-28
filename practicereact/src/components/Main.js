0a5e68f8883830d7478bb7692814b0a561603389
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2024-11-28 20:26:25 +0530

    import image

--- practicereact/src/components/Main.js
+++ practicereact/src/components/Main.js
@@ -1,7 +1,8 @@
-export default function Main(){
+export default function Main({id,name,city}){
     return(
         <div>
-            
+            <img src="https://m.media-amazon.com/images/I/51MnVEDKPwL._AC_.jpg" alt="teddyImage"/>
+            <h1>{name}</h1>
         </div>
     )
 }
